eb5bce670e46b6a94919c9adb44c7d7b2efc8639
Wiktor Kowalczyk <kowalczyk.wiktor@gmail.com>
2019-06-19 13:59:01 +0200

    addingParameters

--- scrappOLX.r
+++ scrappOLX.r
@@ -34,3 +34,49 @@ for( pageIndex in 1 : n){
   urlpage <- wektorLinkow[pageIndex]
   if (is.na (stringr::str_extract(urlpage, 'olx'))){
     print(wektorLinkow[pageIndex])
+    
+    stronahtml <- read_html(urlpage[[1]] )
+    place <- stronahtml %>% html_nodes("a.show-map-link")%>%html_nodes("strong")%>%html_text()
+    idogloszenia <- stronahtml %>% html_nodes("em")%>%html_nodes("small")%>%html_text()
+    print(idogloszenia)
+    
+    data <- stronahtml %>% html_nodes("em") %>% html_text()
+    print(data)
+    
+    textid <- gregexpr(pattern="ID ogloszenia", idogloszenia)
+    print(textid[[1]])
+    
+    aftertext <- stri_sub(idogloszenia,textid[[1]]+14,-1)
+    print(aftertext)
+    idogloszenia <- str_squish(aftertext)
+    print(idogloszenia)
+    
+    tbls_ls <- stronahtml %>%
+      html_nodes("table") %>% .[3:10]%>%html_table(fill=TRUE)
+      ofertaOd <- NA;cenaZam<-NA;poziom<-NA;umeblowanie<-NA;rynek<-NA;rodzajZabudowy<-NA;powierzchnia<-NA;liczbaPokoi<-NA
+      for(i in 1:8){
+        if (grepl("Oferta_od",tbls_ls[[i]]$X1)){
+      ofertaOd <- tbls_ls[[i]]$X2
+  }
+  if (grepl("Cena_za",tbls_ls[[i]]$X1)){
+cenaZam <- tbls_ls[[i]]$X2
+}
+if (grepl("Poziom",tbls_ls[[i]]$X1)){
+poziom <- tbls_ls[[i]]$X2
+}
+if (grepl("Umeblowanie",tbls_ls[[i]]$X1)){
+umeblowanie <- tbls_ls[[i]]$X2
+}
+if (grepl("Rynek",tbls_ls[[i]]$X1)){
+rynek <- tbls_ls[[i]]$X2
+}
+if (grepl("Rodzaj_zabudowy",tbls_ls[[i]]$X1)){
+rodzajZabudowy <- tbls_ls[[i]]$X2
+}
+if (grepl("Powierzchnia",tbls_ls[[i]]$X1)){
+powierzchnia <- tbls_ls[[i]]$X2
+}
+if (grepl("Liczba_pokoi",tbls_ls[[i]]$X1)){
+liczbaPokoi <- tbls_ls[[i]]$X2
+}
+      }
